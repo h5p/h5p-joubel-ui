45a95f084e20b29c7a568e5c0f712b827b216ee8
Svein-Tore Griff With <with@amendor.no>
2015-02-01 22:50:33 +0100

    Add joubel button

--- js/joubel-ui.js
+++ js/joubel-ui.js
@@ -22,5 +22,37 @@ H5P.JoubelUI = (function ($) {
     return new H5P.JoubelMessageDialog($container, message);
   };
   
+  /**
+   * Create standard Joubel button
+   * 
+   * @param {object} params
+   *  May hold the following properties:
+   *   - text
+   *     Button text
+   *   - title
+   *     Title attribute for the button
+   *   - href
+   *     Href attribute for the button (button becomes a tag instead of button)
+   *   - class
+   *     extra classes for the button, multiple values must be separated with
+   *     spaces
+   */
+  JoubelUI.createButton = function(params) {
+    var type = 'button';
+    if (params.href) {
+      type = 'a';
+    }
+    else {
+      params.type = 'button';
+    }
+    if (params.class) {
+      params.class += ' h5p-joubelui-button';
+    }
+    else {
+      params.class = 'h5p-joubelui-button';
+    }
+    return $('<' + type + '/>', params);
+  };
+  
   return JoubelUI;
 })(H5P.jQuery);
