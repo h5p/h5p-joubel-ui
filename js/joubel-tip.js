1f9f31613c1a99721518db0fd4893c10ea368254
Håkon Ellingsen <hakonweb@gmail.com>
2016-09-22 08:12:03 +0200

    HVP-145: Display help tips on hover

--- js/joubel-tip.js
+++ js/joubel-tip.js
@@ -1,6 +1,7 @@
 var H5P = H5P || {};
 
 H5P.JoubelTip = (function ($) {
+  var sticky = false;
 
   /**
    * Creates a new tip
@@ -24,19 +25,60 @@ H5P.JoubelTip = (function ($) {
     var $tip = $('<div/>', {
       class: 'joubel-tip-container' + (params.showSpeechBubble ? '' : ' be-quiet'),
       title: parsedTitle,
-      click: function () {
-        if (speechBubble !== undefined && speechBubble.isCurrent($tip)) {
-          speechBubble.remove();
-          speechBubble = undefined;
-        }
-        else if (params.showSpeechBubble) {
-          speechBubble = H5P.JoubelSpeechBubble($tip, text);
-        }
-        return false;
+      on: {
+        click: function () {
+          // Disable hover and make tip stick for help tip
+          if (params.helpIcon) {
+            if (sticky) {
+              sticky = false;
+              return false;
+            }
+            else {
+              sticky = true;
+              return false;
+            }
+          }
+
+          // Unstick the help tip if other tips are clicked
+          sticky = false;
+          toggleSpeechBubble();
+
+          return false;
+        },
+        mouseenter: mouseHandler,
+        mouseleave: mouseHandler
       }
     }).append($('<div/>', {
       'class': 'joubel-tip-icon' + (params.helpIcon ? ' help-icon': '')
     }));
+
+    /**
+     * Add or remove a speech bubble
+     *
+     * @private
+     */
+    function toggleSpeechBubble() {
+      if (speechBubble !== undefined && speechBubble.isCurrent($tip)) {
+        speechBubble.remove();
+        speechBubble = undefined;
+      }
+      else if (params.showSpeechBubble) {
+        speechBubble = H5P.JoubelSpeechBubble($tip, text);
+      }
+    }
+
+    /**
+     * Toggle a speech bubble on hover
+     *
+     * @private
+     */
+    function mouseHandler() {
+      // Only help tips should have a hover effect
+      if (params.helpIcon && sticky !== true) {
+        toggleSpeechBubble();
+      }
+    }
+
     return $tip;
   }
 
