7f945e1756a8d9f3d9adf85c80e8add72d708511
Thomas Marstrander <marstranderthomas@gmail.com>
2021-11-25 15:02:54 +0100

    HFP-3438 Remove title from tip button

--- js/joubel-tip.js
+++ js/joubel-tip.js
@@ -37,7 +37,6 @@ H5P.JoubelTip = (function ($) {
     // Create Tip button
     var $tipButton = $('<div/>', {
       class: 'joubel-tip-container' + (behaviour.showSpeechBubble ? '' : ' be-quiet'),
-      title: behaviour.tipLabel,
       'aria-label': behaviour.tipLabel,
       'aria-expanded': false,
       role: 'button',
